7869747bb7c38f9f25255b1de9e85e28de8e6fc2
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2018-03-02 14:20:54 +0400

    DDD

--- core/ddd-runtime/src/main/java/io/schoolspointframework/core/ddd/runtime/DddJavassistAgentAutoConfiguration.java
+++ core/ddd-runtime/src/main/java/io/schoolspointframework/core/ddd/runtime/DddJavassistAgentAutoConfiguration.java
@@ -1,22 +1,18 @@
 package io.schoolspointframework.core.ddd.runtime;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
-import org.springframework.instrument.classloading.LoadTimeWeaver;
+import java.lang.instrument.Instrumentation;
 
 import static io.schoolspointframework.core.ddd.runtime.agent.DddJavassistAgentFactory.INSTANCE;
 
 /**
  * @author Bhuwan Prasad Upadhyay
  */
-@Configuration
 public class DddJavassistAgentAutoConfiguration {
 
-    @Bean
-    public LoadTimeWeaver loadTimeWeaver() {
-        InstrumentationLoadTimeWeaver loadTimeWeaver = new InstrumentationLoadTimeWeaver();
-        INSTANCE.transformers().forEach(loadTimeWeaver::addTransformer);
-        return loadTimeWeaver;
+    public static void premain(final String agentArgs,
+                               final Instrumentation inst) {
+        System.out.printf("[Java-Agent]: Starting %s\n", DddJavassistAgentAutoConfiguration.class.getSimpleName());
+        INSTANCE.transformers().forEach(inst::addTransformer);
     }
+
 }
