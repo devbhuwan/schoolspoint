d31286fe955100519c03dd1b191535e33dba8e95
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2018-03-02 19:04:13 +0400

    DDD

--- student/src/main/java/io/schoolspointframework/student/domain/PersistentStudentManager.java
+++ student/src/main/java/io/schoolspointframework/student/domain/PersistentStudentManager.java
@@ -2,12 +2,9 @@ package io.schoolspointframework.student.domain;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
-
-import java.util.Optional;
 
 /**
  * @author Bhuwan Prasad Upadhyay
@@ -24,28 +21,6 @@ class PersistentStudentManager<T extends Student> implements StudentManager<Stud
     public Student save(Student student) {
         Assert.notNull(student, "Student must not be null");
         return studentRepository.save(student);
-    }
-
-    @Override
-    public Optional<Student> get(Long studentIdentifier) {
-        Assert.notNull(studentIdentifier, "studentIdentifier must not be null");
-        return studentRepository.findById(studentIdentifier);
-    }
-
-    @Override
-    public boolean contains(Long studentIdentifier) {
-        Assert.notNull(studentIdentifier, "studentIdentifier must not be null");
-        return studentRepository.existsById(studentIdentifier);
-    }
-
-    @Override
-    public void getByRollNumber(RollNumber rollNumber) {
-
-    }
-
-    @Override
-    public Streamable<Student> getAllByGrade(Grade grade) {
-        return null;
     }
 
 }
--- student/src/main/java/io/schoolspointframework/student/domain/StudentManager.java
+++ student/src/main/java/io/schoolspointframework/student/domain/StudentManager.java
@@ -1,20 +1,8 @@
 package io.schoolspointframework.student.domain;
 
-import org.springframework.data.util.Streamable;
-
-import java.util.Optional;
-
 /**
  * @author Bhuwan Prasad Upadhyay
  */
 public interface StudentManager<T extends Student> {
     T save(T student);
-
-    Optional<T> get(Long studentIdentifier);
-
-    boolean contains(Long studentIdentifier);
-
-    void getByRollNumber(RollNumber rollNumber);
-
-    Streamable<T> getAllByGrade(Grade grade);
 }
