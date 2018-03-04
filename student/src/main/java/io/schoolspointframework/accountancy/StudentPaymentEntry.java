171f7b72559cb480617a7991d59c6c43c7eefc7a
devbhuwan <bhuwan.upadhyay49@gmail.comm>
2018-03-04 19:17:46 +0400

    #5

--- student/src/main/java/io/schoolspointframework/accountancy/StudentPaymentEntry.java
+++ student/src/main/java/io/schoolspointframework/accountancy/StudentPaymentEntry.java
@@ -1,7 +1,14 @@
 package io.schoolspointframework.accountancy;
 
+import io.schoolspointframework.core.ddd.annotations.DddEntity;
+
+import javax.persistence.Entity;
+
 /**
  * @author Bhuwan Prasad Upadhyay
  */
+@Entity
+@DddEntity
 public class StudentPaymentEntry extends AccountancyEntry {
+
 }
--- student/src/test/java/io/schoolspointframework/accountancy/AccountancyIntegrationTests.java
+++ student/src/test/java/io/schoolspointframework/accountancy/AccountancyIntegrationTests.java
@@ -2,11 +2,15 @@ package io.schoolspointframework.accountancy;
 
 import io.schoolspointframework.AbstractIntegrationTests;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author Bhuwan Prasad Upadhyay
  */
 public class AccountancyIntegrationTests extends AbstractIntegrationTests {
+
+    @Autowired
+    private Accountancy accountancy;
 
     @Test
     public void rejectNullEntry() {
